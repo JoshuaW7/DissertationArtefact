1cd4e4a3050b6fab43acee02738baa5738cdbca6
Joshua Wilde <s5115230@bournemouth.ac.uk>
2021-04-28 19:20:48 +0100

    Add project files.

---
+++ Server/Controllers/IncomeController.cs
@@ -0,0 +1,43 @@
+﻿using DissertationArtefact.Shared;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DissertationArtefact.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class IncomeController : ControllerBase
+    {
+        /*private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+        */
+        private readonly ILogger<IncomeController> _logger;
+
+        public IncomeController(ILogger<IncomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Income> Get()
+        {
+            var incomes = new Array<Income>
+            {
+                new Income {Amount = "2000", LabelName = "Salary", Occurance = Occurances.monthly, PaymentDate = "15/12/2020",  SenderName = "Job"},
+                new Income {Amount = "2000", LabelName = "Salary", Occurance = Occurances.monthly, PaymentDate = "15/12/2020",  SenderName = "Job"},
+                new Income {Amount = "2000", LabelName = "Salary", Occurance = Occurances.monthly, PaymentDate = "15/12/2020",  SenderName = "Job"},
+                new Income {Amount = "2000", LabelName = "Salary", Occurance = Occurances.monthly, PaymentDate = "15/12/2020",  SenderName = "Job"},
+                new Income {Amount = "2000", LabelName = "Salary", Occurance = Occurances.monthly, PaymentDate = "15/12/2020",  SenderName = "Job"},
+                new Income {Amount = "2000", LabelName = "Salary", Occurance = Occurances.monthly, PaymentDate = "15/12/2020",  SenderName = "Job"}
+
+            };
+            return incomes;
+        }
+    }
+}
---
+++ Shared/Income.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DissertationArtefact.Shared
+{
+
+    /// <summary>
+    /// Summary description for Class1
+    /// </summary>
+    public class Income
+    {
+        public Income()
+        {
+
+        }
+
+        public string LabelName { get; set; }
+        public string Amount { get; set; }
+        public string SenderName { get; set; }
+        public string PaymentDate { get; set; }
+        public Occurances Occurance { get; set; }
+
+    }
+
+    public enum Occurances { 
+    oneTime,
+    weekly,
+    monthly,
+    yearly
+
+    }
+}
