86015735548ee5238a0d909f25b210566b313ee7
Joshua Wilde <s5115230@bournemouth.ac.uk>
2021-04-29 21:08:46 +0100

    Commit 2: Loose page scaffolding complete

--- Server/Controllers/IncomeController.cs
+++ Server/Controllers/IncomeController.cs
@@ -12,11 +12,6 @@ namespace DissertationArtefact.Server.Controllers
     [Route("[controller]")]
     public class IncomeController : ControllerBase
     {
-        /*private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-        */
         private readonly ILogger<IncomeController> _logger;
 
         public IncomeController(ILogger<IncomeController> logger)
@@ -29,12 +24,12 @@ namespace DissertationArtefact.Server.Controllers
         {
             var incomes = new List<Income>
             {
-                new Income {Amount = 2000, LabelName = "Salary", Occurance = Occurances.monthly, PaymentDate = new DateTime( 2020, 12, 15),  SenderName = "Job"},
-                new Income {Amount = 2000, LabelName = "Salary", Occurance = Occurances.monthly, PaymentDate = new DateTime( 2020, 12, 15),  SenderName = "Job"},
-                new Income {Amount = 2000, LabelName = "Salary", Occurance = Occurances.monthly, PaymentDate = new DateTime( 2020, 12, 15),  SenderName = "Job"},
-                new Income {Amount = 2000, LabelName = "Salary", Occurance = Occurances.monthly, PaymentDate = new DateTime( 2020, 12, 15),  SenderName = "Job"},
-                new Income {Amount = 2000, LabelName = "Salary", Occurance = Occurances.monthly, PaymentDate = new DateTime( 2020, 12, 15),  SenderName = "Job"},
-                new Income {Amount = 2000, LabelName = "Salary", Occurance = Occurances.monthly, PaymentDate = new DateTime( 2020, 12, 15),  SenderName = "Job"}
+                new Income {Amount = 2000, LabelName = "Salary", Occurance = Occurances.monthly, PaymentDate = new DateTime(2020, 12, 15),  SenderName = "Job"},
+                new Income {Amount = 2000, LabelName = "Salary", Occurance = Occurances.monthly, PaymentDate = new DateTime(2020, 12, 15),  SenderName = "Job"},
+                new Income {Amount = 2000, LabelName = "Salary", Occurance = Occurances.monthly, PaymentDate = new DateTime(2020, 12, 15),  SenderName = "Job"},
+                new Income {Amount = 2000, LabelName = "Salary", Occurance = Occurances.monthly, PaymentDate = new DateTime(2020, 12, 15),  SenderName = "Job"},
+                new Income {Amount = 2000, LabelName = "Salary", Occurance = Occurances.monthly, PaymentDate = new DateTime(2020, 12, 15),  SenderName = "Job"},
+                new Income {Amount = 2000, LabelName = "Salary", Occurance = Occurances.monthly, PaymentDate = new DateTime(2020, 12, 15),  SenderName = "Job"}
 
             };
             return incomes;
