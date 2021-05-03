d6ccc9dd6b97aea5cdb1f89a078bc07261d413b9
Joshua Wilde <s5115230@bournemouth.ac.uk>
2021-05-03 21:55:00 +0100

    Commit 7: Full income CRUD

--- Server/Controllers/IncomeController.cs
+++ Server/Controllers/IncomeController.cs
@@ -24,25 +24,7 @@ namespace DissertationArtefact.Server.Controllers
         [HttpGet]
         public List<Income> Get()
         {
-            // User field needed at some point...
-            // Extract to file (Read & Write)
-            //string s = WinOS.File.ReadAllText("");
-            
-            //var incomes = new List<Income>
-            //{
-            //    new Income {IncomeID = Guid.NewGuid(), Amount = 2000, LabelName = "Salary", Frequency = IncomeFrequencies.Monthly, PaymentDate = new DateTime(2020, 12, 15),  SenderName = "Job"},
-            //    new Income {IncomeID = Guid.NewGuid(), Amount = 1200, LabelName = "Property", Frequency = IncomeFrequencies.Monthly, PaymentDate = new DateTime(2020, 12, 15),  SenderName = "Tenant"},
-            //    new Income {IncomeID = Guid.NewGuid(), Amount = 200, LabelName = "Present", Frequency = IncomeFrequencies.OneTime, PaymentDate = new DateTime(2020, 12, 23),  SenderName = "Parents"},
-            //    new Income {IncomeID = Guid.NewGuid(), Amount = 360, LabelName = "Salary", Frequency = IncomeFrequencies.OneTime, PaymentDate = new DateTime(2020, 12, 22),  SenderName = "British Airways"},
-            //    new Income {IncomeID = Guid.NewGuid(), Amount = 25, LabelName = "Salary", Frequency = IncomeFrequencies.OneTime, PaymentDate = new DateTime(2020, 12, 15),  SenderName = "Friend"},
-            //    new Income {IncomeID = Guid.NewGuid(), Amount = 7.50, LabelName = "Salary", Frequency = IncomeFrequencies.OneTime, PaymentDate = new DateTime(2020, 12, 15),  SenderName = "Friend"}
-
-            //};
-            
-            //string incomeJSON = JsonSerializer.Serialize(incomes);
-
-            //System.IO.File.WriteAllText(@$"d:\temp\{ nameof(incomes)}.json", incomeJSON);
-
+          
             string DataFile = @$"d:\temp\incomes.json";
 
             if (!WinOS.File.Exists(DataFile))
@@ -54,12 +36,83 @@ namespace DissertationArtefact.Server.Controllers
 
             List<Income> incomes = JsonSerializer.Deserialize<List<Income>>(incomesJSON);
 
+            // Consider a default sort... date
+
             return incomes;
 
         }
 
+        [HttpPost()]
+        public async Task<IActionResult> Post(Income income)
+        {
+            string DataFile = @$"d:\temp\incomes.json";
+
+            if (!WinOS.File.Exists(DataFile))
+            {
+                return BadRequest();
+            }
+
+            string IncomesJSON = WinOS.File.ReadAllText(DataFile);
+
+            List<Income> incomes = JsonSerializer.Deserialize<List<Income>>(IncomesJSON);
+
+            if (income.IncomeID is null || income.IncomeID.ToString().Length == 0)
+            {
+                income.IncomeID = Guid.NewGuid();
+                incomes.Add(income);
+            }
+            else
+            {
+                Income incomeCurrent = incomes.Where(i => i.IncomeID == income.IncomeID).FirstOrDefault();
+
+                if (incomeCurrent is null)
+                {
+                    return NotFound();
+                }
+
+                incomes.Remove(incomeCurrent);
+                incomes.Add(income);
+
+            }
+
+            IncomesJSON = JsonSerializer.Serialize(incomes);
+            WinOS.File.WriteAllText(DataFile, IncomesJSON);
+
+            // Returns 200
+            return new CreatedResult($"income/{income.IncomeID}", income);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            string DataFile = @$"d:\temp\incomes.json";
+
+            if (!WinOS.File.Exists(DataFile))
+            {
+                return BadRequest();
+            }
+
+            string incomesJSON = WinOS.File.ReadAllText(DataFile);
+
+            List<Income> incomes = JsonSerializer.Deserialize<List<Income>>(incomesJSON);
+
+            Income incomeCurrent = incomes.Where(i => i.IncomeID.ToString() == id).FirstOrDefault();
+            if (incomeCurrent is null)
+            {
+                return NotFound();
+            }
+
+            // Changes when database is implemented...
+            incomes.Remove(incomeCurrent);
+            incomesJSON = JsonSerializer.Serialize(incomes);
+            WinOS.File.WriteAllText(DataFile, incomesJSON);
+
+            return Ok();
+        }
+
+
         [HttpGet("{id}")]
-        public Income GetByID(string id)
+        public async Task<IActionResult> GetByID(string id)
         {
             // User field needed at some point...
             // Extract to file (Read & Write)
@@ -67,56 +120,20 @@ namespace DissertationArtefact.Server.Controllers
 
             string DataFile = @$"d:\temp\incomes.json";
 
-            if (!WinOS.File.Exists(DataFile)) 
+            if (!WinOS.File.Exists(DataFile))
             {
-                return null;
+                return BadRequest();
             }
 
-            if (new Guid().ToString() == id) 
-            {
-                return null;
-            }
+            string IncomesJSON = WinOS.File.ReadAllText(DataFile);
 
-            string incomesJSON = WinOS.File.ReadAllText(DataFile);
-
-            List<Income> incomes = JsonSerializer.Deserialize<List<Income>>(incomesJSON);
+            List<Income> incomes = JsonSerializer.Deserialize<List<Income>>(IncomesJSON);
 
             var income = incomes.Where(i => i.IncomeID == new Guid(id)).FirstOrDefault();
-            return income;
+
+            return new OkObjectResult(income);
 
         }
 
-        // POST
-        // To append the new "Income" object to your existing JSON....
-        // ... Read all of the file/JSON into your LIST
-        // ... Use List.Append(income);
-        // ... Write out the JSON of the LIST..
-
-        // PUT / POST with string ID
-        // ... Read all of the file/JSON into your LIST
-        // ... iterate over the list find the object - assign the values to the object
-        // ... Write out the JSON of the LIST..
-        /*
-                [HttpPost]
-                public void PostIncome (Income Income) 
-                {
-
-                    List<Income> incomes = new List<Income>()
-                    {
-                        new Income {IncomeID = Guid.NewGuid(), Amount = 2000, LabelName = "Salary", Frequency = IncomeFrequencies.Monthly, PaymentDate = new DateTime(2020, 12, 15),  SenderName = "Job"},
-                        new Income {IncomeID = Guid.NewGuid(), Amount = 1200, LabelName = "Property", Frequency = IncomeFrequencies.Monthly, PaymentDate = new DateTime(2020, 12, 15),  SenderName = "Tenant"},
-                        new Income {IncomeID = Guid.NewGuid(), Amount = 200, LabelName = "Present", Frequency = IncomeFrequencies.OneTime, PaymentDate = new DateTime(2020, 12, 23),  SenderName = "Parents"},
-                        new Income {IncomeID = Guid.NewGuid(), Amount = 360, LabelName = "Salary", Frequency = IncomeFrequencies.OneTime, PaymentDate = new DateTime(2020, 12, 22),  SenderName = "British Airways"},
-                        new Income {IncomeID = Guid.NewGuid(), Amount = 25, LabelName = "Salary", Frequency = IncomeFrequencies.OneTime, PaymentDate = new DateTime(2020, 12, 15),  SenderName = "Friend"},
-                        new Income {IncomeID = Guid.NewGuid(), Amount = 7.50, LabelName = "Salary", Frequency = IncomeFrequencies.OneTime, PaymentDate = new DateTime(2020, 12, 15),  SenderName = "Friend"}
-
-                    };
-
-                    string incomeJSON = JsonSerializer.Serialize(incomes);
-
-                   // File.WriteAllText(@$"c:\temp\{ nameof(incomes)}.json", incomeJSON);
-
-
-                }*/
     }
 }
